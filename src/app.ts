718a1a3ed10dde45637e1c99f73a097a0c39f908
410631526 <1qaz2wsx.ud@gmail.com>
2024-12-18 00:47:56 +0800

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ export const DB = new MongoDB({
 });
 
 // 確保從環境變數中讀取前端的 origin URL
-const frontendOrigin = process.env.FRONTEND_URL || "http://localhost:1526"; // 默認使用 http://localhost:1526
+const frontendOrigin = process.env.FRONTEND_URL || "http://localhost:5173"; // 默認使用 http://localhost:5173
 
 app.use(cors({
   origin: frontendOrigin, // 設置具體的前端 URL，而不是 '*'
